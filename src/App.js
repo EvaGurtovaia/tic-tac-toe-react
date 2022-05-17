df0e0a93ee2e2497f530cca29d1cdd2e5e61c521
EvaGurtovaia <eva.gurtovaya@gmail.com>
2022-05-17 17:57:37 -0500

    Start working on tic tac toe game

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { Card, CardBody, Container, Button, Col, Row } from "reactstrap";
+import "bootstrap/dist/css/bootstrap.css";
+import { useState } from "react";
+import "./App.css";
+import Icon from "./components/icon";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const itemsArray = new Array(9).fill("empty");
+
+const App = () => {
+    const [isCrossed, setIsCrossed] = useState(false);
+    const [winMessage, setWinMessage] = useState("");
+
+    const reloadGame = () => {
+        //
+    };
+
+    const checkisWinner = () => {
+        //
+    };
+
+    const changeItem = (itemNumber) => {
+        //
+    };
+
+    return (
+        <div className="App">
+            <Icon />
+        </div>
+    );
+};
 
 export default App;
